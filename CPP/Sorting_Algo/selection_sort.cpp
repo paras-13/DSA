883a97ea233d9cdeef0d3fbf92b8f3b7707bf613
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-09-30 11:08:57 +0530

    Selection Sort Algorithm

--- CPP/Sorting_Algo/selection_sort.cpp
+++ CPP/Sorting_Algo/selection_sort.cpp
@@ -1,8 +1,18 @@
 // Selection Sort algorithm 
 /*
+It is an efficient sorting algorithm which works by repeatedly selecting the smallest 
+(or largest) element from the unsorted portion of the list.
 
+Complexity Analysis:- 
+ -> Time Complexity :- O[n^2]
+ -> Auxiliary Space :- O[1]
+
+ -> Best Case Time Complexity :- O[n^2
+ -> Average Case Time Complexity :- O[n^2]
+ -> Worst case Time Complexity :- O[n^2]
 */
 
+// This sorting method requires the minimum number of swaps
 // Algo -->
 #include <iostream>
 using namespace std;
@@ -25,9 +35,19 @@ int main()
 
 void selectionSort(int* arr, int size)
 {
-    
+    int ind_min;
+    for(int i=0; i<size; i++)
+    {
+        ind_min = i;
+        for(int j=i+1; j<size; j++)
+        {
+            if(arr[ind_min] > arr[j])
+            ind_min = j;
+        }
+        if(ind_min != i) swap(arr[ind_min], arr[i]);
+    }
 }
 void print(int arr[], int size)
 {
-    
+    for(int i=0; i<size; i++) cout << arr[i] << " ";
 }
