cbc198f3f91d1d1cfa891a796aadb472e51aa543
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-10-25 23:59:18 +0530

    Evaluating postfix expression using Stack

---
+++ CPP/Stack/Problems/Easy/postfix_evaluation.cpp
@@ -0,0 +1,13 @@
+// Ques = To evaluate postfix expression using stack 
+#include <iostream>
+#include <stack>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter a postfix expression: ";
+    cin >> str;
+    
+}
