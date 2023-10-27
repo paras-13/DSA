3fb2101cd920125fec728fd840fd8c0e935fe05e
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-10-27 22:09:11 +0530

    Evaluation postfix expression using stack

--- CPP/Stack/Problems/Easy/postfix_evaluation.cpp
+++ CPP/Stack/Problems/Easy/postfix_evaluation.cpp
@@ -1,13 +1,50 @@
-// Ques = To evaluate postfix expression using stack 
+// Ques = To evaluate postfix expression using stack
+/*
+    Expression of the form “a b operator”
+    Time Complexity: O(N) 
+    Auxiliary Space: O(N)
+*/
 #include <iostream>
 #include <stack>
-#include<string>
+#include <string>
 using namespace std;
 
+int evaluate(string str)
+{
+    stack<int> st;
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (isdigit(str[i]))
+            st.push(str[i] - '0');
+        else
+        {
+            int val1 = st.top();
+            st.pop();
+            int val2 = st.top();
+            st.pop();
+            switch (str[i])
+            {
+            case '+':
+                st.push(val2 + val1);
+                break;
+            case '-':
+                st.push(val2 - val1);
+                break;
+            case '*':
+                st.push(val2 * val1);
+                break;
+            case '/':
+                st.push(val2 / val1);
+                break; 
+            }
+        }
+    }
+    return st.top();
+}
 int main()
 {
     string str;
     cout << "Enter a postfix expression: ";
     cin >> str;
-    
+    cout << "Result: " << evaluate(str);
 }
