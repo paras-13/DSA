f7e389d2b785569eaa1e49a0f6a55cfd1c8f928f
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-12-31 23:31:03 +0530

    Spiral Level Order Traversal Binary Tree

---
+++ CPP/Tree/Binary tree/spiral_levelOrder.cpp
@@ -0,0 +1,113 @@
+// Spiral or Zigzag Level Order Traversal in Binary tree
+/*
+Complexity-->
+                Time = O(n)
+                Space = O(n
+)*/
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* right;
+    Node* left;
+    Node(int x)
+    {
+        data = x;
+        right = left = NULL;
+    }
+};
+
+Node* insertNode(Node* root, int val)
+{
+    if(root == NULL) return new Node(val);
+    queue <Node*> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        Node* temp = q.front();
+        q.pop();
+        if(temp->left != NULL)
+            q.push(temp->left);
+        else{
+            temp->left = new Node(val);
+            return root;
+        }
+        if(temp->right!= NULL)
+            q.push(temp->right);
+        else{
+            temp->right = new Node(val);
+            return root;
+        }
+    }
+    return root;
+}
+
+void levelOrder(Node* root)
+{
+    if(root == NULL) return;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        Node* temp = q.front();
+        cout << temp->data << " ";
+        q.pop();
+        if(temp->left!=NULL) q.push(temp->left);
+        if(temp->right!=NULL) q.push(temp->right);
+    }
+}
+
+// Using stack
+void spiralOrder_stack(Node* root)
+{
+    if(root == NULL) return;
+
+    stack<Node*> st1;   // Print right to left
+    stack<Node*> st2;   // Print left to right
+    st1.push(root);
+
+    while(!st1.empty() || !st2.empty())
+    {
+        while(!st1.empty())
+        {
+            Node* temp = st1.top();
+            st1.pop();
+            cout << temp->data <<" ";
+            if(temp->right!=NULL) st2.push(temp->right);
+            if(temp->left!=NULL) st2.push(temp->left);
+        }
+
+        while(!st2.empty())
+        {
+            Node* temp = st2.top();
+            st2.pop();
+            cout << temp->data << " ";
+            if(temp->left!=NULL) st1.push(temp->left);
+            if(temp->right!=NULL) st1.push(temp->right);
+        }
+    }
+
+}
+
+int main()
+{
+    Node* root = NULL;
+    int n, val;
+    cout << "Total elements to enter: ";
+    cin >> n;
+    cout << "Enter elements: ";
+    for(int i=0; i<n; i++)
+    {
+        cin >> val;
+        root = insertNode(root, val);
+    }
+    levelOrder(root); cout << endl;
+    cout << "In spiral order: ";
+    spiralOrder_stack(root);
+}
