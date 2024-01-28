7faf791f38c6191b657064bd1602b9a97a3b61c1
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-01-28 18:18:24 +0530

    Calculating power using Recursion

---
+++ CPP/Recursion/power.cpp
@@ -0,0 +1,18 @@
+// Calculating power using Recursion
+#include <iostream>
+using namespace std;
+int power(int x, int n)
+{
+    if(n==0) return 1;
+    return x*power(x,n-1);
+}
+int main()
+{
+    int x, n;
+    cout << "Enter number: ";
+    cin >> x;
+    cout << "Enter power: ";
+    cin >> n;
+    cout << "Result: "<<power(x, n);
+
+}
