76ef1e8ed0e64b2f3c6fa04048b1c2222494e6af
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-03-21 15:48:09 +0530

    Zigzag Level Order Traversal

--- CPP/Tree/Binary tree/spiral_levelOrder.cpp
+++ CPP/Tree/Binary tree/spiral_levelOrder.cpp
@@ -71,25 +71,24 @@ void spiralOrder_stack(Node* root)
     stack<Node*> st1;   // Print right to left
     stack<Node*> st2;   // Print left to right
     st1.push(root);
-
-    while(!st1.empty() || !st2.empty())
-    {
-        while(!st1.empty())
-        {
-            Node* temp = st1.top();
-            st1.pop();
-            cout << temp->data <<" ";
-            if(temp->right!=NULL) st2.push(temp->right);
-            if(temp->left!=NULL) st2.push(temp->left);
-        }
-
-        while(!st2.empty())
-        {
-            Node* temp = st2.top();
-            st2.pop();
+    bool flag=true;
+    while(!st1.empty()) {
+        Node* temp = st1.top();
+        st1.pop();
+        if(temp) {
             cout << temp->data << " ";
-            if(temp->left!=NULL) st1.push(temp->left);
-            if(temp->right!=NULL) st1.push(temp->right);
+            if(flag) {
+                if(temp->left) st2.push(temp->left);
+                if(temp->right) st2.push(temp->right);
+            }
+            else {
+                if(temp->right) st2.push(temp->right);
+                if(temp->left) st2.push(temp->left);
+            }
+        }
+        if(st1.empty()) {
+            flag = !flag;
+            swap(st1, st2);
         }
     }
 
