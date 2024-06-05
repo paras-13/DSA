68b6417a6e7e3a841f0819e8a910d85256446a3c
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-06-05 12:39:10 +0530

    DP fibonacci series

--- CPP/DP/DP_fibonacci.cpp
+++ CPP/DP/DP_fibonacci.cpp
@@ -2,8 +2,15 @@
 #include <vector>
 using namespace std;
 
+// Normal Recursive
+int fib1(int n) {
+    if(n <=1) return n;
+    int ans = fib1(n-1) + fib1(n-2);
+    return ans;
+}
+
 // DP Memoization
-int fibo(int n, vector <int> &dp) {
+int fib2(int n, vector <int> &dp) {
     
     // base case
     if(n<=1) return n;
@@ -12,12 +19,12 @@ int fibo(int n, vector <int> &dp) {
     if(dp[n] != -1) return dp[n];
     
     // final case
-    dp[n] =  fibo(n-1, dp) + fibo(n-2, dp);
+    dp[n] =  fib2(n-1, dp) + fib2(n-2, dp);
     return dp[n];
 }
 
 // DP Tabulation
-int fib(int n, vector<int> &dp) {
+int fib3(int n, vector<int> &dp) {
     dp[0] = 0;
     dp[1] = 1;
     for(int i=2; i<=n; i++) {
@@ -25,11 +32,34 @@ int fib(int n, vector<int> &dp) {
     }
     return dp[n];
 }
+
+// Space Optimization
+int fib4(int n) {
+    int first = 0;
+    int second = 1;
+    int third;
+    for(int i=2; i<=n; i++) {
+        third = first+second;
+        first = second;
+        second = third;
+    }
+    return third;
+}
 int main() {
     int n;
     cin >> n;
     vector <int> dp(n+1);
     for(int i=0; i<=n; i++) dp[i] = -1;
-    cout << fibo(n, dp) << endl;
-    cout << fib(n, dp);
+
+    // Recursive
+    cout << fib1(n) << endl;  
+
+    // Top - Down  :- O(N + N), O(N)
+    cout << fib2(n, dp) << endl;
+
+    // Bottom - Up    O(N), O(N)
+    cout << fib3(n, dp) << endl;
+
+    // Space Optimized      O(N),   O(1);
+    cout << fib4(n) << endl;
 }
