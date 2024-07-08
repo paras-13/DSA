ba91f3d1a4ae91c20984f102bafb73f3d9270762
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-07-08 22:37:43 +0530

    Traversing a 2D matrix

---
+++ CPP/2D Matrix/matrixTraversal.cpp
@@ -0,0 +1,21 @@
+// Matrix Traversal :- Traverse all elements in the matrix;
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[3][4] = {
+                        {1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12}
+                    };
+
+    cout << "Matrix: \n";
+    // Traversing over all the rows
+    for(int i=0; i<3; i++) {
+        // Traversing over all columns
+        for(int j=0; j<4; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
