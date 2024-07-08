b57e492fc7f6a2edfb9f760e6483bb396b23bb23
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-07-08 22:32:32 +0530

    2D matrix declaration

---
+++ CPP/2D Matrix/matrixDeclaration.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Defining rows and columns;
+    int rows = 3;
+    int columns = 3;
+
+    // Array Declaration
+    int arr[rows][columns];
+    return 0;
+}
