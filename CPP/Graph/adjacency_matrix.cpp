d05bab694af7c27146a793af96cd2a5bff8e2134
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-07-09 12:49:04 +0530

    Adjacency matrix Representation of Graph

--- CPP/Graph/adjacency_matrix.cpp
+++ CPP/Graph/adjacency_matrix.cpp
@@ -1,27 +1,43 @@
+// Adjacency Matrix Representation Of Graph
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main()
-{
+class Graph {
+    private:
+    vector<vector<int>> adjMatrix;
+
+    public:
+    Graph(int v) {
+        adjMatrix.resize(v+1, vector<int>(v+1, 0));
+    }
+    void addEdge(int u, int v) {
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1;    
+    }
+    void print() {
+        cout << "Adjacency Matrix: " << endl;
+        for(int i=0; i<adjMatrix.size(); i++) {
+            for(int j=0; j<adjMatrix[i].size(); j++) 
+                cout << adjMatrix[i][j] << " ";
+            cout << endl;
+        }
+    }
+};
+
+int main() {
     int v, e;
-    cin >> v >> e;
+    cout << "Enter number of vertices: ";
+    cin >> v;
+    cout << "Enter number of edges: ";
+    cin >> e;
+    Graph g1(v);
 
-    vector<vector<int>> matrix(v, vector<int>(v, 0)); 
-    for(int i = 0; i < e; i++)
-    {
-        int fv, sv;
-        cin >> fv >> sv;
-        matrix[fv][sv] = 1;
-        matrix[sv][fv] = 1;
+    cout << "Enter edge between vertices (u v): " << endl;
+    for(int i=0; i<e; i++) {
+        int u, v;
+        cin >> u >> v;
+        g1.addEdge(u, v);
     }
-    cout << "Adjacency Matrix:" << endl;
-    for(int i = 0; i < v; i++)
-    {
-        for(int j = 0; j < v; j++)
-            cout << matrix[i][j] << " ";
-        cout << endl;
-    }
-
-    return 0;
+    g1.print();
 }
