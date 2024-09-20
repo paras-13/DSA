f370e3866624666b41f78213d02c10c411744a4e
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-09-20 17:53:03 +0530

    Longest Common Subsequence (DP)

--- CPP/DP/longestCommonSubsequence.cpp
+++ CPP/DP/longestCommonSubsequence.cpp
@@ -35,7 +35,20 @@ int memo(string &s1, string &s2, int n, int m, vector<vector<int>> &dp) {
 // Using Tabulation
 // Time Complexity: O(n*m);
 int tabulation(string s1, string s2, int n, int m) {
-
+    vector<vector<int>>dp(n+1, vector<int>(m+1));
+    for(int i=0; i<=m; i++)
+        dp[i][0] = 0;
+    for(int i=0; i<=m; i++)
+        dp[0][i] = 0;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            if(s1[i-1] == s2[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else 
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+        }
+    }
+    return dp[n][m];
 }
 int lcs(string s1, string s2, int n, int m) {
     // Recursive Approach
