217beb4ca07944a1b76171133866ff511b87d317
Paras Upadhyay <paras13upadhyay@gmail.com>
2025-02-11 20:41:29 +0530

    Insertion Sort

--- CPP/Sorting_Algo/insertion_sort.cpp
+++ CPP/Sorting_Algo/insertion_sort.cpp
@@ -1,47 +1,47 @@
 // Algorithm for insertion sort
 /*
-    Time Complexity analysis:- 
+    Time Complexity analysis:-
      --> Worst case Analysis:- O[n^2]
      --> Average time Complexity :- O[n^2];
      --> Best Time Complexity :- O[n];
 
      -> Auxiliary - Space :- O[1];
 */
- // Algo -->
+// Algo -->
 #include <iostream>
+#include <vector>
 using namespace std;
-void insertion_sort(int* arr, int size);
-void print(int arr[], int size);
-
+void insertionSort(vector<int> &arr)
+{
+    int j;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        j = i - 1;
+        while (j >= 0 && arr[j] > arr[j + 1])
+        {
+            swap(arr[j], arr[j + 1]);
+            j--;
+        }
+    }
+}
+void printArr(vector<int> &arr)
+{
+    for (int it : arr)
+        cout << it << " ";
+}
 int main()
 {
     int n;
-    cout << "Enter number of elements: "; cin >> n;
-    int arr[n];
-    cout << "Enter elements: ";
-    for(int i=0; i<n; i++) cin >> arr[i];
-    cout << "Original: "; print(arr, n);
-    insertion_sort(arr, n);
-    cout << endl << "After sorting: "; print(arr, n);
-    return 0;
-}
-
-void insertion_sort(int* arr, int size)         // Sorting Algo
-{
-    int key, j;
-    for(int i=0; i<size; i++)
-    {
-        key = arr[i];
-        j = i-1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
-
-void print(int arr[], int size)
-{
-    for(int i=0; i<size; i++) cout << arr[i] << " ";
+    cout << "Enter number of elements to enter: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << "Original arry: ";
+    printArr(arr);
+    insertionSort(arr);
+    cout << endl
+         << "Sorted array: ";
+    printArr(arr);
 }
