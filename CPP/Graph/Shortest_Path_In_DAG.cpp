cad77943d6fa4d492bcf79ccb5d3292d45592302
Paras Upadhyay <paras13upadhyay@gmail.com>
2025-08-27 16:58:57 +0530

    Shortest Path in DAG

--- CPP/Graph/Shortest_Path_In_DAG.cpp
+++ CPP/Graph/Shortest_Path_In_DAG.cpp
@@ -1,4 +1,4 @@
-// Shortest Path in a Directed Acyclic Graph: 
+// Shortest Path in a Directed Acyclic Graph:
 // Time Complexity :- TopoSort=O(V+E); shortPath:- O(V+E) , Net = O(V+E);
 #include <iostream>
 #include <vector>
@@ -6,49 +6,61 @@
 #include <stack>
 #include <algorithm>
 using namespace std;
-class Graph {
-    private:
+class Graph
+{
+private:
     vector<vector<pair<int, int>>> adj;
     int v;
-    public:
-    Graph(int v) {
+
+public:
+    Graph(int v)
+    {
         this->v = v;
         adj.resize(v);
     }
-    void addEdge(int u, int v, int w) {
+    void addEdge(int u, int v, int w)
+    {
         adj[u].push_back({v, w});
     }
     stack<int> getTopoSort();
-    void DFS(stack<int>&st, vector<bool>&visited, int s);
-    vector<int> shortestPathInDAG(stack<int>&st, int s);
+    void DFS(stack<int> &st, vector<bool> &visited, int s);
+    vector<int> shortestPathInDAG(stack<int> &st, int s);
 };
 
-void Graph :: DFS(stack<int>&st, vector<bool>&visited, int s) {
+void Graph ::DFS(stack<int> &st, vector<bool> &visited, int s)
+{
     visited[s] = true;
-    for(auto it : adj[s]) {
-        if(!visited[it.first])
+    for (auto it : adj[s])
+    {
+        if (!visited[it.first])
             DFS(st, visited, it.first);
     }
     st.push(s);
 }
-stack<int> Graph ::  getTopoSort() {
-    stack<int>st;
-    vector<bool>visited(v+1, false);
-    for(int i=0; i<v; i++){
-        if(!visited[i])
+stack<int> Graph ::getTopoSort()
+{
+    stack<int> st;
+    vector<bool> visited(v + 1, false);
+    for (int i = 0; i < v; i++)
+    {
+        if (!visited[i])
             DFS(st, visited, i);
     }
     return st;
 }
 
-vector<int> Graph :: shortestPathInDAG(stack<int>&st, int s){
-    vector<int>distance(v, INT_MAX);
+vector<int> Graph ::shortestPathInDAG(stack<int> &st, int s)
+{
+    vector<int> distance(v, INT_MAX);
     distance[s] = 0;
-    while(!st.empty()) {
+    while (!st.empty())
+    {
         int node = st.top();
         st.pop();
-        if(distance[node]!=INT_MAX) {
-            for(auto it : adj[node]) {
+        if (distance[node] != INT_MAX)
+        {
+            for (auto it : adj[node])
+            {
                 int v = it.first;
                 int w = it.second;
                 distance[v] = min(distance[v], distance[node] + w);
@@ -57,7 +69,8 @@ vector<int> Graph :: shortestPathInDAG(stack<int>&st, int s){
     }
     return distance;
 }
-int main() {
+int main()
+{
     int v, e;
     cout << "Enter number of vertices: ";
     cin >> v;
@@ -65,19 +78,22 @@ int main() {
     cin >> e;
     Graph g1(v);
     cout << "Enter edges with weight: ";
-    for(int i=0; i<e; i++) {
+    for (int i = 0; i < e; i++)
+    {
         int x, y, w;
         cin >> x >> y >> w;
         g1.addEdge(x, y, w);
     }
-    stack<int>st = g1.getTopoSort();
+    stack<int> st = g1.getTopoSort();
     int s;
     cout << "Enter souce: ";
     cin >> s;
-    vector<int>distance = g1.shortestPathInDAG(st, s);
+    vector<int> distance = g1.shortestPathInDAG(st, s);
     cout << "Minimum cost path: ";
-    for(int i=0; i<distance.size(); i++) {
-        if(distance[i] == INT_MAX) cout << -1 << " ";
+    for (int i = 0; i < distance.size(); i++)
+    {
+        if (distance[i] == INT_MAX)
+            cout << -1 << " ";
         else
             cout << distance[i] << " ";
     }
